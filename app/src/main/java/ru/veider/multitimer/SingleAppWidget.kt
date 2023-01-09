0c57e60d461b13566060051532b3cfb0f5acb7c9
Корховой Вадим <veider@inbox.ru>
2023-01-09 20:28:39 +0300

    22. Переработана отрисовка виджета

--- app/src/main/java/ru/veider/multitimer/SingleAppWidget.kt
+++ app/src/main/java/ru/veider/multitimer/SingleAppWidget.kt
@@ -6,12 +6,10 @@ import android.appwidget.AppWidgetProvider
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
-import android.graphics.*
 import android.os.Build
 import android.view.View
 import android.widget.RemoteViews
 import ru.veider.multitimer.const.toShortTime
-import kotlin.math.max
 
 class SingleAppWidget : AppWidgetProvider() {
 
@@ -30,12 +28,6 @@ class SingleAppWidget : AppWidgetProvider() {
             val pendingIntent = getPendingIntent(context)
             val views = RemoteViews(context.packageName, R.layout.single_app_widget).apply {
                 setOnClickPendingIntent(R.id.widgetText, pendingIntent)
-                setImageViewBitmap(R.id.widgetProgressBody,
-                                   getWidgetBitmap(context,
-                                                   getWidgetSize(context, widgetId).toInt(),
-                                                   1.0f
-                                   )
-                )
             }
 
             appWidgetManager.updateAppWidget(widgetId, views)
@@ -71,12 +63,7 @@ class SingleAppWidget : AppWidgetProvider() {
                             views.apply {
                                 setViewVisibility(R.id.widgetText, View.VISIBLE)
                                 setViewVisibility(R.id.widgetBell, View.GONE)
-                                setImageViewBitmap(R.id.widgetProgressBody,
-                                                   getWidgetBitmap(context,
-                                                                   getWidgetSize(context, widgetId).toInt(),
-                                                                   currentTime.toFloat() / maxTime
-                                                   )
-                                )
+                                setProgressBar(R.id.progress_bar, maxTime, currentTime, false)
                                 setTextViewText(R.id.widgetText, currentTime.toShortTime())
                             }
                             widgetManager.updateAppWidget(widgetId, views)
@@ -86,12 +73,7 @@ class SingleAppWidget : AppWidgetProvider() {
                         for (widgetId in widgetIds) {
                             views.apply {
                                 setViewVisibility(R.id.widgetText, View.GONE)
-                                setImageViewBitmap(R.id.widgetProgressBody,
-                                                   getWidgetBitmap(context,
-                                                                   getWidgetSize(context, widgetId).toInt(),
-                                                                   1.0f
-                                                   )
-                                )
+                                setProgressBar(R.id.progress_bar, 100, 100, false)
                                 setViewVisibility(R.id.widgetBell, View.VISIBLE)
                             }
 
@@ -103,12 +85,7 @@ class SingleAppWidget : AppWidgetProvider() {
                             views.apply {
                                 setViewVisibility(R.id.widgetText, View.VISIBLE)
                                 setViewVisibility(R.id.widgetBell, View.GONE)
-                                setImageViewBitmap(R.id.widgetProgressBody,
-                                                   getWidgetBitmap(context,
-                                                                   getWidgetSize(context, widgetId).toInt(),
-                                                                   1.0f
-                                                   )
-                                )
+                                setProgressBar(R.id.progress_bar, 100, 100, false)
                                 setTextViewText(R.id.widgetText, 0.toShortTime())
                             }
                             widgetManager.updateAppWidget(widgetId, views)
@@ -130,67 +107,7 @@ class SingleAppWidget : AppWidgetProvider() {
                                           PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
                 )
 
-    private fun getWidgetSize(context: Context, widgetId: Int): Float {
-        val providerInfo = AppWidgetManager.getInstance(context).getAppWidgetInfo(widgetId)
-        var widgetLandWidth = providerInfo.minWidth
-        var widgetPortHeight = providerInfo.minHeight
-        var widgetPortWidth = providerInfo.minWidth
-        var widgetLandHeight = providerInfo.minHeight
-        AppWidgetManager.getInstance(context).getAppWidgetOptions(widgetId)?.run {
-            if (getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH) > 0) {
-                widgetPortWidth = getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH)
-                widgetLandWidth = getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_WIDTH)
-                widgetLandHeight = getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT)
-                widgetPortHeight = getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_HEIGHT)
-            }
-        }
-        val maxSize = max(max(widgetPortWidth, widgetLandWidth), max(widgetPortHeight, widgetLandHeight))
-
-        return context.dip(maxSize).toFloat()
-    }
-
-    private fun Context.dip(value: Int): Int = (value * resources.displayMetrics.density).toInt()
-
-    private fun getWidgetBitmap(context: Context, size: Int, percentage: Float): Bitmap {
-        val strokeWidth = context.resources.getInteger(R.integer.widget_stroke_width) / 100.0f * size
-
-
-        val strokeBold = strokeWidth * 1.5f
-        val arc = RectF().apply {
-            set(strokeBold / 2, strokeBold / 2, size - strokeBold / 2, size - strokeBold / 2)
-        }
-
-//        val paintBold = Paint(Paint.FILTER_BITMAP_FLAG and Paint.DITHER_FLAG and Paint.ANTI_ALIAS_FLAG).apply {
-//            setStrokeWidth(strokeBold)
-//            style = Paint.Style.STROKE
-//            strokeCap = Paint.Cap.ROUND
-//            color = context.resources.getColor(R.color.color_on_primary, context.theme)
-//        }
-        val paintLight = Paint(Paint.FILTER_BITMAP_FLAG and Paint.DITHER_FLAG and Paint.ANTI_ALIAS_FLAG).apply {
-            setStrokeWidth(strokeWidth)
-            style = Paint.Style.STROKE
-            strokeCap = Paint.Cap.ROUND
-            color =
-                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
-                        context.resources.getColor(R.color.color_primary)
-                    else
-                        context.resources.getColor(R.color.color_primary, context.theme)
-        }
-
-        val bitmap = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888)
-        val canvas = Canvas(bitmap)
-
-        val startAngle = 270 - 360 * percentage
-        canvas.drawArc(arc, startAngle, 360 * percentage, false, paintLight)
-
-        return bitmap
-    }
-
     override fun onEnabled(context: Context) {
-        onReceive(context, Intent())// Enter relevant functionality for when the first widget is created
-    }
-
-    override fun onDisabled(context: Context) {
-        // Enter relevant functionality for when the last widget is disabled
+        onReceive(context, Intent())
     }
 }
